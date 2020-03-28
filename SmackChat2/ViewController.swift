c9ffa1dc1124d4873a1fd6314a355823c5d740c9
vishal chaubey <Vishal@sachins-macbook-pro.local>
2020-03-28 09:55:58 +0530

    Initial Commit

---
+++ SmackChat2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackChat2
+//
+//  Created by vishal chaubey on 3/28/20.
+//  Copyright © 2020 DeviSons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
